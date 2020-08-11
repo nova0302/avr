ba275fa39ed41e87d6bb45b3770b0378d6771346
nova0302 <nova0302@hotmail.com>
2020-08-11 13:25:46 +0900

    Add files via upload

--- main2.c
+++ main2.c
@@ -1,48 +1,44 @@
 /*
- * blinky.c
- *
- * Created: 2020-08-10 오전 10:54:57
- * Author : TEACHER
- */ 
+* blinky.c
+*
+* Created: 2020-08-10 오전 10:54:57
+* Author : TEACHER
+*/
 #define F_CPU 8000000UL
 #include <avr/io.h>
 #include <util/delay.h>
 
 int main(void)
-{	
-	uint8_t bPattern[10] ={
-		0b11111001 // 0
-		,0b11111001 //1 
-		,0b11111001 //2
-		,0b11111001 //3
-		,0b11111001
-		,0b11111001
-		,0b11111001
-		,0b11111001
-		,0b11111001
-		,0b11111001 //9
+{
+	uint8_t bP0[10] ={
+		0b11111001 		,0b11111001
+		,0b11111001 	,0b11111001
+		,0b11111001		,0b11111001
+		,0b11111001		,0b11111001
+		,0b11111001		,0b11111001
+	};
+	uint8_t bP1[10] ={
+		0b11111001 			,0b11111001
+		,0b11111001 		,0b11111001
+		,0b11111001			,0b11111001
+		,0b11111001			,0b11111001
+		,0b11111001			,0b11111001
 	};
 	// DDRx, PORTx, PINx
 	// port b 0번째 비트를 출력으로 설정
 	DDRA = (1 << DDA2) | (1 << DDA1);
-	PORTA &= ~(1 << DDA1);// 00000010 
+	PORTA &= ~(1 << DDA1);// 11111101
 	DDRC = 0xFF;
-    while (1) 
-    {	
-		if(PINB & (1 << PINB1)){//버튼이 눌려지지 않았을 경우
-			for(int i = 0; i < 10; i++)
-			{
-				PORTC = bPattern[i];
-				_delay_ms(1000);
-			}	
-		}
-		else
+	while (1)
+	{
+		for(int i = 0; i < 10; i++)
 		{
-			for(int i = 9; i >= 0; i--)
-			{
-				PORTC = bPattern[i];
-				_delay_ms(1000);
-			}
+			PORTC = bP0[i];
+			PORTA &= ~(1 << DDA1);// 11111101
+			_delay_ms(10);
+			PORTC = bP1[i];
+			PORTA &= ~(1 << DDA2);// 11111101
+			_delay_ms(10);
 		}
 	}
 }
