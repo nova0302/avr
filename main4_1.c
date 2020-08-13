5c1deaa6960a21b95149aaa3832fe9cb0c85c4a6
nova0302 <nova0302@hotmail.com>
2020-08-13 15:58:22 +0900

    Add files via upload

--- main4_1.c
+++ main4_1.c
@@ -18,27 +18,30 @@ void stepStm(EMotDir dir)
 		if (++theCounter > 3) {
 			theCounter = 0;
 		}
-		}else{
+	}else{
 		if (--theCounter < 0) {
 			theCounter = 3;
 		}
 	}
-
+	PORTD &= 0b10000111;
 	switch (theCounter) {
-		case 0: {PORTD &= 0b10001111; break;}
-		case 1: {PORTD &= 0b10010111; break;}
-		case 2: {PORTD &= 0b10100111; break;}
-		case 3: {PORTD &= 0b11000111; break;}
+		case 0: {PORTD |= 0b00001000; break;}
+		case 1: {PORTD |= 0b00010000; break;}
+		case 2: {PORTD |= 0b00100000; break;}
+		case 3: {PORTD |= 0b01000000; break;}
 		default: break;
 	}
 }
 int main(void)
 {
+	DDRB = (1 << DDB0);
+	
 	// port D의 6:3번째 비트를 출력으로 설정
 	DDRD |= (1 << DDD6) |(1 << DDD5)| (1 << DDD4)| (1 << DDD3);
 	/* Replace with your application code */
 	while (1)
-	{
+	{	
+		PORTB ^= 1;
 		stepStm(CW);
 		_delay_ms(100);
 	}
